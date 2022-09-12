0413dc433318f815ab2a394dab5ee536e4f61999
Ted Havelka <thavelka@cornellpump.com>
2022-09-12 15:48:16 -0700

    WIP - preparing to parse response packets based on details of each type of response in MCUBOOTRM.pdf.

--- nxpbl.py
+++ nxpbl.py
@@ -29,26 +29,31 @@ from mcuboot_packets import *
 #
 # - SECTION - references
 #
-#  (1)  https://www.tutorialspoint.com/increment-and-decrement-operators-in-python
+#   (1)  https://www.tutorialspoint.com/increment-and-decrement-operators-in-python
 #
-#  (2)  https://www.geeksforgeeks.org/print-without-newline-python/
+#   (2)  https://www.geeksforgeeks.org/print-without-newline-python/
 #
-#  (3)  https://www.askpython.com/python-modules/python-time-sleep-method 
+#   (3)  https://www.askpython.com/python-modules/python-time-sleep-method 
 #
-#  (4)  https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.write
+#   (4)  https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.write
 #
-#  (5)  https://gist.github.com/yptheangel/fcd62ad59a569ace75eb07025b8e9c4f . . . serialPort.write(bytes.fromhex("a5"))
+#   (5)  https://gist.github.com/yptheangel/fcd62ad59a569ace75eb07025b8e9c4f . . . serialPort.write(bytes.fromhex("a5"))
 #
-#  (6)  https://jimmywongiot.com/2021/03/13/byte-manipulation-on-python-platform/
+#   (6)  https://jimmywongiot.com/2021/03/13/byte-manipulation-on-python-platform/
 #
 #  Note STM32WL55 flash memory start mapped to 0x08000000, per document:
-#  (7)  rm0453-stm32wl5x-advanced-armbased-32bit-mcus-with-subghz-radio-solution-stmicroelectronics.pdf
+#   (7)  rm0453-stm32wl5x-advanced-armbased-32bit-mcus-with-subghz-radio-solution-stmicroelectronics.pdf
 #
 #
 # 2022-09-07
 #
-#  (8)  https://learn.adafruit.com/micropython-basics-loading-modules/import-code
+#   (8)  https://learn.adafruit.com/micropython-basics-loading-modules/import-code
 #
+#   (9)  https://docs.python.org/3/library/stdtypes.html . . . int.from_bytes(value, byteorder="little")
+#
+#  (10)  http://dabeaz.blogspot.com/2010/01/few-useful-bytearray-tricks.html
+#
+#  (11)  https://www.alpharithms.com/python-bytearray-built-in-function-123516/
 #
 #
 # Tangential topics:
@@ -58,12 +63,30 @@ from mcuboot_packets import *
 # ----------------------------------------------------------------------
 #
 
-# alternate baudrates tested:  115200 - b'\xff' responses
-#                                9600 - no response at either delay 1uS, 100uS
-#                               38400 - no response at 1uS
-#
 
-SERIAL_PORT_READ_TIMEOUT = 1.0 # seconds
+# ----------------------------------------------------------------------
+# - SECTION - effective constants
+# ----------------------------------------------------------------------
+
+ONE_NANOSECOND           = 0.000000001
+ONE_MICROSECOND          = 0.000001
+TEN_MICROSECONDS         = 0.00001
+ONE_HUNDRED_MICROSECONDS = 0.0001
+
+#CHOSEN_DELAY = ONE_HUNDRED_MICROSECONDS
+#CHOSEN_DELAY = TEN_MICROSECONDS
+CHOSEN_DELAY = ONE_NANOSECOND
+
+SERIAL_PORT_READ_TIMEOUT = 0.00004340 # in seconds . . . was 1.0 seconds
+
+DISPLAY_BYTE_PER_LINE = 1
+DISPLAY_PACKET_PER_LINE = 2
+
+
+
+# ----------------------------------------------------------------------
+# - SECTION - primary script scoped variables
+# ----------------------------------------------------------------------
 
 serialPort = serial.Serial(port = "/dev/ttyUSB0",
                            baudrate=115200,
@@ -87,21 +110,6 @@ HANDSHAKE_ATTEMPTS_TO_MAKE = 4     # . . . was 20
 command_to_bootloader = 0
 command_get_attempts = 0
 COMMAND_GET_ATTEMPTS_TO_MAKE = 2
-
-
-
-# ----------------------------------------------------------------------
-# - SECTION - constants ( in primary bootloader client script )
-# ----------------------------------------------------------------------
-
-ONE_NANOSECOND           = 0.000000001
-ONE_MICROSECOND          = 0.000001
-TEN_MICROSECONDS         = 0.00001
-ONE_HUNDRED_MICROSECONDS = 0.0001
-
-#CHOSEN_DELAY = ONE_HUNDRED_MICROSECONDS
-#CHOSEN_DELAY = TEN_MICROSECONDS
-CHOSEN_DELAY = ONE_NANOSECOND
 
 
 
@@ -229,6 +237,72 @@ def memory_address_with_crc(address):
 
 
 # ----------------------------------------------------------------------
+#  @brief   Routine - first draft - to parse mcuboot response packets
+# ----------------------------------------------------------------------
+
+def parse_packet(packet):
+
+#    print("Parser:  got packet of %u bytes to parse," % len(packet))
+#    print("  zzzzz")
+
+    if (1):
+        print(" ", end=" ")
+        for byte in packet:
+            print("0x%02x" % int.from_bytes(byte, byteorder='little'), end=" ")
+        print()
+
+# - CHECK - for 'ACK' type packet . . .
+    if(len(packet) == 2):
+        if((int.from_bytes(packet[1], byteorder='little') == MCUBOOT_BOOTLOADER_START_BYTE) and
+           (int.from_bytes(packet[0], byteorder='little') == MCUBOOT_FRAMING_PACKET_TYPE__ACK)):
+            print("parser - DEV 0912 - packet is 'basic ack' type.")
+
+#    print("  zzzzz")
+
+
+
+# ----------------------------------------------------------------------
+#  @brief   Routine to display response from MCU bootloader.
+# ----------------------------------------------------------------------
+
+def listen_for_mcuboot_response(display_option):
+
+    bootloader_response = []
+    response_previous = []
+
+#    print("listener routine called to parse response . . .")
+    print("LISTENER ROUTINE CALLED TO PARSE RESPONSE . . .")
+    while ( serialPort.in_waiting == 0 ):
+        time.sleep(CHOSEN_DELAY)
+
+    while ( serialPort.in_waiting > 0 ):
+        val = serialPort.read()
+        bootloader_response.append(val)
+
+# When we detect an MCUBoot start byte then add a white space line for clarity of output:
+        if ( val[0] == 0x5a ):
+
+# Possible point of response packet processing here . . .
+            print()
+            print("received response of %u" % len(bootloader_response), end=" bytes.\n")
+            response_previous = []
+            response_previous.extend(bootloader_response)
+            print("copy of latest mcuboot response - ", response_previous)
+            bootloader_response = []
+            parse_packet(response_previous)
+            print()
+
+        if (display_option == DISPLAY_BYTE_PER_LINE):
+            print("0x%02x" % val[0], end=" ")
+            print(val)
+        else:
+            print("0x%02x" % val[0], end=" ")
+
+    print()
+
+
+
+# ----------------------------------------------------------------------
 # - SECTION - main line code
 # ----------------------------------------------------------------------
 
@@ -341,14 +415,32 @@ if (1):
 
     while ( serialPort.in_waiting > 0 ):
         serialString = serialPort.read()
+
+# When we detect an MCUBoot start byte then add a white space line for clarity of output:
         if ( serialString[0] == 0x5a ):
             print()
-#        print(hex(serialString[0]), end=" ")
+
 # Reference https://stackoverflow.com/questions/61206672/how-to-add-leading-0-to-hexadecimal
         val = serialString[0]
         print("0x%02x" % val, end=" ")
         print(serialString)
 
+    print()
+
+
+# ----------------------------------------------------------------------
+# DEV TEST 5:
+# ----------------------------------------------------------------------
+
+    print("DEV - sending 'read memory' command . . .")
+    send_command_bootloader_nxp(command_as_bytes, len(command_as_bytes))
+
+    listen_for_mcuboot_response(DISPLAY_PACKET_PER_LINE)
+
+
+
+
+    print("")
     print("- INFO - response to ping command appears done.")
     print("- STEP - reading serial port once more as a timeout test,")
     print("       ( timeout set to", end=" ")
